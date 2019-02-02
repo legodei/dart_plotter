c62ef67377e8ecdee181aa19078eed83c3212d3a
legodei <42155055+legodei@users.noreply.github.com>
2019-02-02 14:58:37 -0800

    Add files via upload

--- densityplot.py
+++ densityplot.py
@@ -3,27 +3,100 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import kde
 import seaborn as sns
+from scipy.stats import norm
 
-x = nightrifle[:,0]
-y = nightrifle[:,1]
- 
-x2 = final3[:,0]
-y2 = final3[:,1]
-# library & dataset
+def centroidnp(arr):
+	''' calculates the centroid given an array of points '''
+	assert arr.shape[1] == 2
+	length = arr.shape[0]
+	sum_x = np.sum(arr[:, 0])
+	sum_y = np.sum(arr[:, 1])
+	return sum_x/length, sum_y/length
+
+def radiusvalue(arr):
+	''' returns a list of radiuses given an array of points '''
+	assert arr.shape[1] == 2
+	x2 = np.power(arr[:,0],2)
+	y2 = np.power(arr[:,1],2)
+	return np.power(x2+y2,0.5)
+	
+	
+Files = [ # Enter Data files here
+'metal_1',
+'rifle_1',
+]
+
+el = np.zeros((0,2),np.int32) # empty list
+
+raw_all = {'rifle':[],'metal':[],'straight':[]} # raw input data
+zeroed_all = {'rifle':el,'metal':el,'straight':el}
+radius_all = {'rifle':[],'metal':[],'straight':[]} 
+
+for filename in Files:
+	if 'rifle' in filename:
+		raw_all['rifle'].append(np.loadtxt(filename+'.csv', delimiter=","))
+	if 'metal' in filename:
+		raw_all['metal'].append(np.loadtxt(filename+'.csv', delimiter=","))
+	if 'straight' in filename:
+		raw_all['rifle'].append(np.loadtxt(filename+'.csv', delimiter=","))
+
+for muzzle in list(raw_all.keys()):
+	for samplelist in raw_all[muzzle]:
+		cx,cy = centroidnp(samplelist)
+		zeroed_list = np.transpose(np.array([samplelist[:,0]-cx,samplelist[:,1]-cy]))
+		zeroed_all[muzzle] = np.append(zeroed_all[muzzle],zeroed_list,axis=0)
+
+for muzzle in list(zeroed_all.keys()):
+	radius_list = list(radiusvalue(zeroed_all[muzzle]))
+	radius_all[muzzle].extend(radius_list)
+		
+sns.distplot(radius_all['metal'], color="blue", label="metal")
+ax = sns.distplot(radius_all['rifle'], color="green", label="rifle")
+plt.legend()
+ax.set_xlim(0,200)
+plt.show()
 
 
-# Change shape of marker
-#sns.regplot(x, y, marker='o', fit_reg=False)
-#sns.plt.show()
-
-# You can see all possible shapes:
-#all_shapes=markers.MarkerStyle.markers.keys()
-#all_shapes
-#[0, 1, 2, 3, 4, u'D', 6, 7, 8, u's', u'|', 11, u'None', u'P', 9, u'x', u'X', 5, u'_', u'^', u' ', None, u'd', u'h', u'+', u'*', u',', u'o', u'.', u'1', u'p', u'3', u'2', u'4', u'H', u'v', u'', u'8', 10, u'&lt;', u'&gt;']
-
-# More marker customization:
-ax = plt.figure()
-sns.regplot(x, y,marker='s', fit_reg=False, scatter_kws={"color":"red","alpha":0.2,"s":100},label="rifled" )
-sns.regplot(x2, y2, marker='D', fit_reg=False, scatter_kws={"color":"green","alpha":0.2,"s":100},label="smoothbore" )
-ax.legend()
-#sns.plt.show()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
--- trajectory.py
+++ trajectory.py
@@ -3,6 +3,9 @@
 Created on Thu Jan 31 14:00:49 2019
 
 @author: Haojie
+
+Processes an mp4 file with trajectory information in it and outputs
+a .wav sound file and also a .csv file with the coordinates. 
 """
 
 import matplotlib.pyplot as plt
@@ -10,55 +13,69 @@ import numpy as np
 import wave
 from scipy.signal import find_peaks
 import cv2
+from pydub import AudioSegment
 
-#Filter Parameters
-audio_thresh = 29000
+# Input Parameters
+audio_thresh = 22000 # Audio Cutoff Threshold for shot recognition (see plot)
+file_name = 'metal_1' # Video file name in mp4 format
+video_thresh = 50 # Brightness threshold for dart detection (0-255)
+shot_gap = 1.7 # minimum number of seconds between shots
 
+# Variable Generation Based on Inputs
+video_file = file_name+'.mp4'
 
-# Process the Audio File
-spf = wave.open('smoothbore.wav','r') #Insert file name here.
+# Extract audio file from video, save it in wav format
+audio = AudioSegment.from_file(video_file,'mp4')
+audio.export(file_name+'.wav','wav')
+
+# Open the Audio File
+sound = wave.open(file_name + '.wav','r') #Insert file name here.
 
 # Extract Raw Audio from Wav File
-signalraw = spf.readframes(-1) # read the audio data into a buffer
-audiorate = spf.getframerate() # find the audio frame rate for timestamping
-signalraw = abs(np.frombuffer(signalraw, np.int16,)) # signal is the raw audio file
-signal1 = signalraw[::2]
-signal2 = signalraw[1::2]
-signal = signal1/2+signal2/2
+signal_raw = sound.readframes(-1) # read the audio data into a buffer
+audio_rate = sound.getframerate() # find the audio frame rate for timestamping
+signal_raw = abs(np.frombuffer(signal_raw, np.int16,)) # signal is the raw audio file
+signal_right = signal_raw[::2] # gets every 2nd element starting with 0, right ch.
+signal_left = signal_raw[1::2] # gets every 2nd element starting with 1, left ch.
+signal = signal_right/2+signal_left/2 # finds the average of the two channels.
+sound.close() #closes sound file
 
-#Find Local Maxima corresponding to firing events.
-shot_times,_ = find_peaks(signal,height = 22000,distance = int(audiorate*1.7))
-shot_times_sec = shot_times/audiorate
+# Find Local Maxima corresponding to firing events.
+shot_times,_ = find_peaks(signal,height = audio_thresh,distance = int(audio_rate*shot_gap))
+shot_times_sec = shot_times/audio_rate # timestamp of shot in seconds
 
-plt.figure(1)
+# Plots Identified shot times for verification.
+plt.figure(1) #places and 'x' on every peak it thinks is a shot'
 plt.title('Shot Times')
-plt.plot(np.array(list(range(len(signal))))/audiorate,signal)
+plt.plot(np.array(list(range(len(signal))))/audio_rate,signal)
 plt.plot(shot_times_sec, signal[shot_times], "x")
 plt.show()
 
-cap = cv2.VideoCapture('smoothbore.mp4')
+cap = cv2.VideoCapture(video_file)
+video_rate = cap.get(cv2.CAP_PROP_FPS)
+
 shotnumber = 0
 fontsize = 0.4
-getout = True
-threshval = 50
+escape = False
 
-trajectories = []
+trajectories = [] # Initialize Trajectories
 
-for tstmp in shot_times_sec:
-	trajcoordlist = np.zeros((1,2),np.int32)
+for tstmp in shot_times_sec: # Loop over shots
+	trajcoordlist = np.zeros((0,2),np.int32)
 	shotnumber+=1
-	if not getout:
+	if escape:
 		break
+	over = False
 	for i in range(120):
-		if not getout:
+		if escape or over:
 			break
-		cap.set(1,int(tstmp*240-20+i))
+		cap.set(1,int(tstmp*video_rate-20+i))
 		ret, frame = cap.read()
 		crop = frame[400:900,660:1260]
 		crop2 = crop.copy()
 		gray = cv2.cvtColor(crop,cv2.COLOR_BGR2GRAY)
 		gray[250:500,:]=0;
-		thresh = cv2.threshold(gray, threshval, 255, cv2.THRESH_BINARY)[1]
+		thresh = cv2.threshold(gray, video_thresh, 255, cv2.THRESH_BINARY)[1]
 		(_,cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 			cv2.CHAIN_APPROX_NONE)
 		max_area = 1
@@ -70,38 +87,30 @@ for tstmp in shot_times_sec:
 				M = cv2.moments(dart)
 				cx = int(M['m10']/M['m00'])
 				cy = int(M['m01']/M['m00'])
-				if np.size(trajcoordlist)<3:
-					trajcoordlist = np.array([[cx,cy]])
-					trajcoordlist = np.append(trajcoordlist, np.array([[cx,cy]]),axis=0)
-				else:
-					trajcoordlist = np.append(trajcoordlist, np.array([[cx,cy]]),axis=0)
-		
-			
+		if max_area == 1 and i>40:
+			over = True  
+		if max_area != 1:	
+			trajcoordlist = np.append(trajcoordlist, np.array([[cx,cy]]),axis=0)
 
 		for trail in trajectories:
 			trail = trail.reshape((-1,1,2))
 			cv2.polylines(crop,[trail],False,(0,0,50),1,lineType=4)
 		if np.size(trajcoordlist)>3:
-			cv2.polylines(crop,[trajcoordlist.reshape((-1,1,2))],False,(0,80,80),1,lineType=4)
-		cv2.putText(crop, "SHOT # "+str(shotnumber),(10,20),cv2.FONT_HERSHEY_SIMPLEX,fontsize, (255,255,0),1)
+			cv2.polylines(crop,[trajcoordlist.reshape((-1,1,2))],False,(0,100,100),2,lineType=4)
+		cv2.putText(crop, "SHOT: "+str(shotnumber),(10,20),cv2.FONT_HERSHEY_SIMPLEX,fontsize, (255,255,0),1)
+		cv2.putText(crop, "FRAME: "+str(i),(10,40),cv2.FONT_HERSHEY_SIMPLEX,fontsize, (255,255,0),1)	  
 		thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
 		if len(cnts):
 			cv2.circle(crop,(cx,cy),15,(100,130,0))
-			
-		
 		monitor = np.hstack((crop,crop2))
-
 		cv2.imshow('frame',monitor)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
-			getout = False
+			escape = True
 	trajectories.append(trajcoordlist)
 cap.release()
 cv2.destroyAllWindows()
 
-final3 = trajectories[0]
+output = np.zeros((0,2),np.int32)
 for path in trajectories:
-	if np.array_equal(final3, path):
-		pass
-	else:
-		final3 = np.append(final3,path,axis=0)
-final3 = final3[:-1,:]
+		output = np.append(output,path,axis=0)
+np.savetxt(file_name+'.csv', output, delimiter=",")
